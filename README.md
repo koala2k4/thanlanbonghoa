fb71acdd27556998cfe13e6364920be205b5c5a4
koala2k4 <59249480+koala2k4@users.noreply.github.com>
2022-03-08 22:14:51 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# thanlanbonghoa
+# testingtesst
+ 
---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Chúc mừng 8/3",
+    introTitle: 'Hỡi đồng bào thằn lằn yêu dấu',
+    introDesc: `Trái đất vốn lạ thường\n
+    Mà sao em cứ đi nhầm đường\n
+    Lạc vào tim anh lẻ loi\n
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^HiHi^^',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'IU THẰN LẰN NHÌU LÉM <33',
+    btnNo: 'MÃI GHÉT THẰN LẰN:3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Iu thằn lằn lun đúng vì nó chưa bao giờ sai <3',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Một ngày là anh em. Mãi mãi là anh em',
+    btnAccept: 'Mãi iu thằn nằn <3',
+    messLink: 'https://www.messenger.com/t/5408440219167926' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
