d12af2d13c1251a5cdea53b3db5284066e9a4980
koala2k4 <59249480+koala2k4@users.noreply.github.com>
2022-03-09 10:50:40 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -28,6 +28,8 @@ function init() {
 }
 
 function firstQuestion() {
+	var audio = new Audio('sound/sound.mp3');
+    audio.play();
     $('.content').hide();
     Swal.fire({
         title: CONFIG.introTitle,
