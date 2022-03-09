8f815f3237580a3105fc44d8cde31b8fb88a673a
koala2k4 <59249480+koala2k4@users.noreply.github.com>
2022-03-09 10:57:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
     titleWeb: "Chúc mừng 8/3",
     introTitle: 'Hỡi đồng bào thằn lằn yêu dấu',
-    introDesc: `Trái đất vốn lạ thường\n
-    Mà sao em cứ đi nhầm đường\n
-    Lạc vào tim anh lẻ loi\n
+    introDesc: `Trái đất vốn lạ thường \r\n
+    Mà sao em cứ đi nhầm đường \r\n
+    Lạc vào tim anh lẻ loi \r\n
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
@@ -11,10 +11,10 @@ const CONFIG = {
     btnYes: 'IU THẰN LẰN NHÌU LÉM <33',
     btnNo: 'MÃI GHÉT THẰN LẰN:3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Click diệu kì💞',
-    reply: 'Iu thằn lằn lun đúng vì nó chưa bao giờ sai 😍😍',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Iu thằn lằn lun đúng vì nó chưa bao giờ sai <3',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Một ngày là anh em. Mãi mãi là anh em 🤟🤟🤟',
+    messDesc: 'Một ngày là anh em. Mãi mãi là anh em',
     btnAccept: 'Mãi iu thằn nằn <3',
-    messLink: 'https://www.messenger.com/t/5408440219167926' //link mess của các bạn. 
+    messLink: 'https://www.messenger.com/t/5408440219167926' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
